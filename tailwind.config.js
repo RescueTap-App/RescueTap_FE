bdbdaaeb49369b968c10081675501b2285808996
Ike Elvis Ndubuisi <provictor.ie@gmail.com>
2024-11-18 12:00:18 +0100

    feat: add tailwind styling and define routes

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //* Always update this to include paths to all components files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
