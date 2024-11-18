b70010092eb9f2305d70c5f97f29e29b078adc69
Ike Elvis Ndubuisi <provictor.ie@gmail.com>
2024-11-19 00:20:01 +0100

    add more routes

---
+++ app/(app)/(tabs)/vehicle-history.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Page() {
+  return (
+    <View className="bg-white flex h-full">
+      <Text className="text-3xl text-red-600">page</Text>
+    </View>
+  );
+}
